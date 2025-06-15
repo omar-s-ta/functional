5e2a1215eaa635ca74152f101d807191c0d4feea
omar-s-ta <omar.sebres@gmail.com>
2025-06-15 13:32:04 +0200

    Update root README

---
+++ ocaml/datastructures/test/tlist_test.ml
@@ -0,0 +1,39 @@
+open Datastructures 
+open QCheck
+
+let rec as_tlist = function
+  | [] -> Tlist.Nil
+  | h :: t -> Tlist.Cons (h, as_tlist t)
+
+let rec as_list = function
+  | Tlist.Nil -> []
+  | Tlist.Cons (h, t) -> h :: as_list t
+
+let gen_int_list   = Gen.list Gen.int
+let gen_float_list = Gen.list Gen.float
+
+let gen_int_tlist   = gen_int_list   |> Gen.map as_tlist
+let gen_float_tlist = gen_float_list |> Gen.map as_tlist
+
+let gen_int_tlist_tlist =
+  Gen.list gen_int_tlist
+
+let test_failure f =
+  try
+    ignore @@ f ();
+    false
+  with
+    | _ -> true
+
+let test_tail =
+  QCheck.Test.make ~name:"tail" (QCheck.make gen_int_tlist)
+    (function
+    | Tlist.Nil ->
+        test_failure (fun () -> Tlist.tail Tlist.Nil)
+    | Tlist.Cons (h, t) ->
+        let tail = Tlist.tail (Tlist.Cons (h, t)) in
+        tail = t)
+
+let tests = [test_tail]
+
+let () = QCheck_runner.run_tests_main tests
